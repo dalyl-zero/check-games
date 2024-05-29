65946dc60d38a76250468624c8051e629805be17
SmfCoder <smfcoder17@gmail.com>
2024-05-29 12:42:34 -0400

    - commented out CardService in app.component - added utilities/card component - updated home page content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,29 @@
-import { Component } from '@angular/core';
-import { CardService } from './services/card.service';
-import { switchMap, tap } from 'rxjs';
-import { HomeComponent } from './components/home/home.component';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CardService } from "./services/card.service";
+import { switchMap, tap } from "rxjs";
+import { HomeComponent } from "./components/home/home.component";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HomeComponent],
-  templateUrl: "app.component.html",
-  styles: []
+    selector: "app-root",
+    standalone: true,
+    imports: [RouterOutlet, HomeComponent],
+    templateUrl: "app.component.html",
+    styles: [],
 })
 export class AppComponent {
-  constructor(private CardService: CardService) {
-    this.CardService.shuffleCards()
-    .pipe(
-      tap(response => {
-        console.log(response);
-      }),
-      switchMap(() => {
-        return this.CardService.distributeCards(4)
-      })
-    )
-    .subscribe(response => {
-      console.log(response);
-      
-    })
-  }
+    constructor(private CardService: CardService) {
+        // this.CardService.shuffleCards()
+        // .pipe(
+        //   tap(response => {
+        //     console.log(response);
+        //   }),
+        //   switchMap(() => {
+        //     return this.CardService.distributeCards(4)
+        //   })
+        // )
+        // .subscribe(response => {
+        //   console.log(response);
+        // })
+    }
 }
